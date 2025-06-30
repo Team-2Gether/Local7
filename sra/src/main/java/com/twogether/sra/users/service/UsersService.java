ce84f4a51a7c8b74ef36bb6cf9040cd2950b73f9
THINKPAD\ssqod <ssqod1202@gmail.com>
2025-06-30 10:01:32 +0900

    add users

---
+++ sra/src/main/java/com/twogether/sra/users/service/UsersService.java
@@ -0,0 +1,4 @@
+package com.twogether.sra.users.service;
+
+public interface UsersService {
+}
---
+++ sra/src/main/java/com/twogether/sra/users/vo/UsersVO.java
@@ -0,0 +1,7 @@
+package com.twogether.sra.users.vo;
+
+import lombok.Data;
+
+@Data
+public class UsersVO {
+}
