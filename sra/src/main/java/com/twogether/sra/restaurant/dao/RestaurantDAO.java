d375b9d1a6c00b71c630bdded18663cb296f2e8f
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-07-04 14:02:11 +0900

    .

--- sra/src/main/java/com/twogether/sra/restaurant/dao/RestaurantDAO.java
+++ sra/src/main/java/com/twogether/sra/restaurant/dao/RestaurantDAO.java
@@ -1,8 +1,4 @@
 package com.twogether.sra.restaurant.dao;
 
-<<<<<<< HEAD:sra/src/main/java/com/twogether/sra/restaurant/dao/RestauransDAO.java
-public class RestauransDAO {
-=======
 public class RestaurantDAO {
->>>>>>> b4368c18947b71d29b836530fdcd838a4dad61bc:sra/src/main/java/com/twogether/sra/restaurant/dao/RestaurantDAO.java
 }
