4e94e43da4d48ba5a9a300109470085cb51e3935
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-07-06 22:24:57 +0900

    LoginForm.js

--- sra-frontend/src/pages/login/LoginForm.js
+++ sra-frontend/src/pages/login/LoginForm.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import useLogin from './hooks/useLogin'; // useLogin 훅 경로
-import AppLogo from './components/AppLogo'; // AppLogo 컴포넌트 경로
-import LoginFormLinks from './components/LoginFormLinks'; // LoginFormLinks 컴포넌트 경로
-import IntroButton from './components/IntroButton'; // IntroButton 컴포넌트 경로
-import LoginFormComponent from './components/LoginFormComponent'; // LoginFormComponent 컴포넌트 경로
 import '../../assets/css/LoginForm.css';
+import AppLogo from './components/AppLogo';
+import LoginFormComponent from './components/LoginFormComponent';
+import LoginFormLinks from './components/LoginFormLinks';
+import IntroButton from './components/IntroButton';
+import useLogin from './hooks/useLogin';
 
 function LoginForm({ onLoginSuccess }) {
-  const navigate = useNavigate(); // 이 부분에 '줄'이 나타나는 것으로 보입니다.
-  const { credential, setCredential, password, setPassword, handleSubmit } = useLogin(onLoginSuccess);
+  const {
+    credential,
+    setCredential,
+    password,
+    setPassword,
+    handleSubmit,
+  } = useLogin(onLoginSuccess);
 
   return (
     <div className="login-form-container">
-      <AppLogo /> {/* AppLogo 컴포넌트 사용 */}
-
+      <AppLogo />
       <LoginFormComponent
         credential={credential}
         setCredential={setCredential}
         password={password}
         setPassword={setPassword}
         handleSubmit={handleSubmit}
-      /> {/* LoginFormComponent 사용 */}
-
-      <LoginFormLinks /> {/* LoginFormLinks 컴포넌트 사용 */}
-
-      <IntroButton /> {/* IntroButton 컴포넌트 사용 */}
+      />
+      <LoginFormLinks />
+      <IntroButton />
     </div>
   );
 }
