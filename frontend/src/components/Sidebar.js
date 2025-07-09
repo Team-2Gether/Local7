5aee1927e848276916fbd5b08949c6989bf598e9
KIM <kimjhoun92@naver.com>
2025-07-09 17:51:39 +0900

    restaurant: RestaurantVote.js, RestaurantVote.css 파일 추가
    Sidebar.js : pick 버튼 추가(아이콘이미지 임시 변경)
    
    Home.js : case 'pick', RestaurantVote 컴포넌트추가

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,37 +1,56 @@
 import React from 'react';
-import {FaHome, FaPlus, FaUser, FaRobot} from 'react-icons/fa'; // 아이콘 임포트
+import { FaHome, FaPlus, FaUser, FaRobot, FaAward } from 'react-icons/fa'; // 아이콘 임포트
 import '../assets/css/Sidebar.css'; // Sidebar.css 임포트 추가
 
-const Sidebar = ({onMenuItemClick}) => {
-    return (
-        <div className="sidebar-container">
-            {/* style 대신 className 사용 */}
-            <button className="sidebar-button"
-                // style, onMouseEnter, onMouseLeave 제거, className 사용
-                onClick={() => onMenuItemClick('home')}>
-                <FaHome className="sidebar-icon"/> {/* style 대신 className 사용 */}
-                <span>홈</span>
-            </button>
-            <button className="sidebar-button"
-                // style, onMouseEnter, onMouseLeave 제거, className 사용
-                onClick={() => onMenuItemClick('add')}>
-                <FaPlus className="sidebar-icon"/> {/* style 대신 className 사용 */}
-                <span>추가</span>
-            </button>
-            <button className="sidebar-button"
-                // style, onMouseEnter, onMouseLeave 제거, className 사용
-                onClick={() => onMenuItemClick('mypage')}>
-                <FaUser className="sidebar-icon"/> {/* style 대신 className 사용 */}
-                <span>마이</span>
-            </button>
-            <button className="sidebar-button"
-                // style, onMouseEnter, onMouseLeave 제거, className 사용
-                onClick={() => onMenuItemClick('ai')}>
-                <FaRobot className="sidebar-icon"/> {/* style 대신 className 사용 */}
-                <span>AI</span>
-            </button>
-        </div>
-    );
+const Sidebar = ({ onMenuItemClick }) => {
+  return (
+    <div className="sidebar-container">
+      {/* style 대신 className 사용 */}
+      <button
+        className="sidebar-button"
+        // style, onMouseEnter, onMouseLeave 제거, className 사용
+        onClick={() => onMenuItemClick('home')}
+      >
+        <FaHome className="sidebar-icon" /> {/* style 대신 className 사용 */}
+        <span>홈</span>
+      </button>
+
+      {/* pick 버튼: 이달의 여행지 순위 투표 ------------------------------*/}
+      <button
+        className="sidebar-button"
+        // style, onMouseEnter, onMouseLeave 제거, className 사용
+        onClick={() => onMenuItemClick('pick')}
+      >
+        <FaAward className="sidebar-icon" /> {/* style 대신 className 사용 */}
+        <span>TOP10</span>
+      </button>
+      {/* --------------------------------------------------------------- */}
+      <button
+        className="sidebar-button"
+        // style, onMouseEnter, onMouseLeave 제거, className 사용
+        onClick={() => onMenuItemClick('add')}
+      >
+        <FaPlus className="sidebar-icon" /> {/* style 대신 className 사용 */}
+        <span>추가</span>
+      </button>
+      <button
+        className="sidebar-button"
+        // style, onMouseEnter, onMouseLeave 제거, className 사용
+        onClick={() => onMenuItemClick('mypage')}
+      >
+        <FaUser className="sidebar-icon" /> {/* style 대신 className 사용 */}
+        <span>마이</span>
+      </button>
+      <button
+        className="sidebar-button"
+        // style, onMouseEnter, onMouseLeave 제거, className 사용
+        onClick={() => onMenuItemClick('ai')}
+      >
+        <FaRobot className="sidebar-icon" /> {/* style 대신 className 사용 */}
+        <span>AI</span>
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
