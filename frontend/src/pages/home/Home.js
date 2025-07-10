f92ca942e6f7d52778b5a6a066dd5f902bd88f06
human <0168101411@naver.com>
2025-07-10 17:55:22 +0900

    ,

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,31 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Sidebar from "../../components/Sidebar";
 import HomeCardFeed from "./HomeCardFeed";
 
 function Home({ currentUser }) {
-  const navigate = useNavigate();
-
-  const handleMenuItemClick = (menuItem) => {
-    console.log(`${menuItem} 메뉴 클릭됨`);
-    if (menuItem === "home") {
-      navigate("/");
-    } else if (menuItem === "pick") {
-    } else if (menuItem === "posts") {
-      navigate("/posts");
-    } else if (menuItem === "mypage") {
-    } else if (menuItem === "ai") {
-    }
-  };
 
   return (
     <div className="main-content-area">
-      <Sidebar onMenuItemClick={handleMenuItemClick} />
 
       <div className="page-content">
-        {/* 여기서 작업 하면 됨 */}
+
         <HomeCardFeed />
+
       </div>
+
     </div>
   );
 }
