79fada230bcd12cfc03c20a11fa9f282caf616c6
guswo4324 <phj2394324@naver.com>
2025-07-14 14:55:30 +0900

    [fix] ip 범위 확장

--- backend/src/main/java/com/twogether/local7/config/WebConfig.java
+++ backend/src/main/java/com/twogether/local7/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("CORS Configuration is being applied!");
         registry.addMapping("/**") // 모든 API 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // React 개발 서버의 출처 허용
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 자격 증명 (쿠키, 인증 헤더 등) 허용
