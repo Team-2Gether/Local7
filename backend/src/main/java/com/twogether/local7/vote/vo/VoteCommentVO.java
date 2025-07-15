319d26323e3741258cea479bd960539472e10f0d
KIM <kimjhoun92@naver.com>
2025-07-15 12:53:13 +0900

    vote 벡인드 퐅더 파일 이름 리팩토링, 작업중

--- backend/src/main/java/com/twogether/local7/vote/vo/VoteCommentVO.java
+++ backend/src/main/java/com/twogether/local7/vote/vo/VoteCommentVO.java
@@ -1,6 +1,6 @@
 package com.twogether.local7.vote.vo;
 
-public class CommentVO {
+public class VoteCommentVO {
     private int createdId;
     private String content;
     private String createdData;
