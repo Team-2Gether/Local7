a9996bf666719aad2ff6e85e9d2be1d5d45067af
human <0168101411@naver.com>
2025-07-16 12:53:21 +0900

    로고 완성본

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,11 +10,16 @@ function Navbar({ isLoggedIn, userNickname, onLogout }) {
 
   return (
     <nav className="navbar">
-     <div className="navbar-brand">
+     <div class="navbar-brand">
         <Link to="/">
-          <span className="local">LOCAL</span>
-          <span className="r7">R7</span>
-        </Link>
+        <span class="l">L</span>
+        <span class="o">O</span>
+        <span class="c">C</span>
+        <span class="a">A</span>
+        <span class="l2">L</span>
+        <span class="r">R</span>
+        <span class="seven">7</span>
+      </Link>
       </div>
       <ul className="navbar-nav">
         {isLoggedIn ? (
