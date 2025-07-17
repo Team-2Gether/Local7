f5d7cc24a0a63d8b5fdea7f40fb59a71ce592872
KIM <kimjhoun92@naver.com>
2025-07-17 13:22:05 +0900

    프론트 vote : 수정 및 이미지 추가

--- backend/src/main/java/com/twogether/local7/vote/service/VoteServiceImpl.java
+++ backend/src/main/java/com/twogether/local7/vote/service/VoteServiceImpl.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 @Service
 public class VoteServiceImpl implements VoteService{
-
+    //공통
     @Autowired
     private VoteDAO voteDAO;
-
+    //지역
     @Override
     public List<VoteVO> getAllRegions() {
         return voteDAO.getAllRegions();
     }
-
+    //게시글
     @Override
     public List<VoteVO> getAllPosts() {
         return voteDAO.getAllPosts();
