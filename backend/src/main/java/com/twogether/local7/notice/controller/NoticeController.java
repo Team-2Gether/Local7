58eb8b5a66c3344c95b6507686de1d0315ac7796
guswo4324 <phj2394324@naver.com>
2025-07-19 00:31:11 +0900

    [fix] 공지사항 해결 완료

--- backend/src/main/java/com/twogether/local7/notice/controller/NoticeController.java
+++ backend/src/main/java/com/twogether/local7/notice/controller/NoticeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notice")
+@RequestMapping("api/notice")
 public class NoticeController {
 
     @Autowired
