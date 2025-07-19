8318360409ffa717f5150fa409b4e03bed1ad5ae
guswo4324 <phj2394324@naver.com>
2025-07-20 02:24:37 +0900

    배치 파일 주석 처리(배치잡 사용 안할  예정)

--- crawling/run_crawler.bat
+++ crawling/run_crawler.bat
@@ -1,10 +1,10 @@
-@echo off
-rem 파이썬 인터프리터의 전체 경로를 입력하세요.
-set PYTHON_PATH="C:\Users\admin\AppData\Local\Programs\Python\Python313\python.exe"
+@REM @echo off
+@REM rem 파이썬 인터프리터의 전체 경로를 입력하세요.
+@REM set PYTHON_PATH="C:\Users\admin\AppData\Local\Programs\Python\Python313\python.exe"
 
-rem 스크립트 파일의 전체 경로를 입력하세요.
-set SCRIPT_PATH="D:\3차 프로젝트\local7\crawling\seven.py"
+@REM rem 스크립트 파일의 전체 경로를 입력하세요.
+@REM set SCRIPT_PATH="D:\3차 프로젝트\local7\crawling\seven.py"
 
-%PYTHON_PATH% %SCRIPT_PATH%
+@REM %PYTHON_PATH% %SCRIPT_PATH%
 
-pause
+@REM pause
